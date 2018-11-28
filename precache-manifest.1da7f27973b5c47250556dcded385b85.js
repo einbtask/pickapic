3487e49c444cc33abc41da5b260c563d623d62e8
Einat <einat@github.com>
2018-11-28 21:59:50 +0200

    Updates

---
+++ precache-manifest.1da7f27973b5c47250556dcded385b85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e58bc4601623eb16bd60",
+    "url": "/pickapic/static/js/runtime~main.e58bc460.js"
+  },
+  {
+    "revision": "2f2ec87cf601cc4a53c7",
+    "url": "/pickapic/static/js/main.2f2ec87c.chunk.js"
+  },
+  {
+    "revision": "5860132ca2020c53966a",
+    "url": "/pickapic/static/js/1.5860132c.chunk.js"
+  },
+  {
+    "revision": "2f2ec87cf601cc4a53c7",
+    "url": "/pickapic/static/css/main.0ecf910c.chunk.css"
+  },
+  {
+    "revision": "a7235bd0a5a486da1fc66f966d36c096",
+    "url": "/pickapic/index.html"
+  }
+];
